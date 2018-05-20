bdb20e1ef0aa52dae36801d5fee9a4c44a07b692
Darryl Fernandes <darrylfernandes@gmail.com>
2018-05-20 20:54:22 +1000

    ShiftState class : shift_state will be a dictionary

--- model/nurse.py
+++ model/nurse.py
@@ -16,9 +16,9 @@ class ShiftState(object):
             Initializes the Nurse's Working Shift State
             shift_state:      Depending on the start day of the month, the day of the week on which the shift
                                     was assigned will be marked.
-                                    {"2018-05-20":"Morning", "2018-05-21":None, "2018-05-22": "Night", ...]
+                                    {"2018-05-20":"Morning", "2018-05-21":None, "2018-05-22": "Night", ...}
             night_shifts_per_month: Incremented by 1 each time a nurse is assigned a night shift
         """
-        self.shift_state = []
+        self.shift_state = {}
         self.night_shifts_per_month = 0
 
