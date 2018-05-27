7281301931881339b575a42c8fe55f319eebfe16
Darryl Fernandes <darrylfernandes@gmail.com>
2018-05-27 10:51:57 +1000

    1) Introduced new RuleException class and a Rule Execution Wrapper to capture exceptions in the rule trace 2) Added Conditional Rules in the Rule object which will be evaluated first prior to execution of the rule 3) Modified the And / Or Rule execution behavior

--- rules_engine/rules/conditional_rules.py
+++ rules_engine/rules/conditional_rules.py
@@ -1,4 +1,4 @@
-from .rule import Rule
+from rules_engine.rules.rule import Rule, RuleException, wrap_rule_exception
 
 
 class And(Rule):
@@ -6,19 +6,44 @@ class And(Rule):
         """
 
         :param rule_id: Unique Rule Id
+        :param rule_description: Rule Description
         :param lhs: Left Hand Side Rule in the AND operation eg:  (rule1 and rule2 where rule1 is lhs rule)
         :param rhs: Right Hand Side Rule in the AND operation eg:  (rule1 and rule2 where rule2 is rhs rule)
         """
-        super(And, self).__init__(rule_id, rule_description)
+        super(And, self).__init__(rule_id, rule_description, conditional_rules=None)
         self.lhs = lhs
         self.rhs = rhs
 
-    def execute(self, rule_input):
+    @wrap_rule_exception
+    def execute(self, rule_input, parent_rule=None):
+        parent_rule_id = parent_rule.rule_id if parent_rule else None
+        parent_rule_description = parent_rule.rule_description if parent_rule else None
+
         if (self.lhs and isinstance(self.lhs, Rule)) and (self.rhs and isinstance(self.rhs, Rule)):
-            return self.rule_id, self.lhs.execute(rule_input) and self.rhs.execute(rule_input), "Pass"
+            lhs_rule_id, lhs_rule_status, lhs_rule_status_reason, lhs_parent_rule_id, lhs_parent_rule_description, \
+                lhs_child_rules = self.lhs.execute(rule_input, parent_rule=self)
+            rhs_rule_id, rhs_rule_status, rhs_rule_status_reason, rhs_parent_rule_id, rhs_parent_rule_description, \
+                rhs_child_rules = self.rhs.execute(rule_input, parent_rule=self)
+            overall_status = lhs_rule_status and rhs_rule_status
+            if overall_status:
+                return self.rule_id, self.rule_description, True, "Pass", \
+                           parent_rule_id, parent_rule_description, \
+                           [(lhs_rule_id, lhs_rule_status, lhs_rule_status_reason,
+                             lhs_parent_rule_id, lhs_parent_rule_description, lhs_child_rules),
+                            (rhs_rule_id, rhs_rule_status, rhs_rule_status_reason,
+                             rhs_parent_rule_id, rhs_parent_rule_description, rhs_child_rules)]
+            else:
+                return self.rule_id, self.rule_description, False, "Fail", \
+                       parent_rule_id, parent_rule_description, \
+                       [(lhs_rule_id, lhs_rule_status, lhs_rule_status_reason,
+                         lhs_parent_rule_id, lhs_parent_rule_description, lhs_child_rules),
+                        (rhs_rule_id, rhs_rule_status, rhs_rule_status_reason,
+                         rhs_parent_rule_id, rhs_parent_rule_description, rhs_child_rules)]
         else:
-            return self.rule_id, False, "The left-hand/right-hand side should be of type Rule. " \
-                          "Kindly validate the Rule Config input for Rule Id {} and try again".format(self.rule_id)
+            return self.rule_id, self.rule_description, False, \
+                   "The left-hand/right-hand side should be of type Rule. " \
+                   "Kindly validate the Rule Config input for Rule Id {} and try again".format(self.rule_id), \
+                   parent_rule_id, parent_rule_description, []
 
 
 class Or(Rule):
@@ -26,16 +51,43 @@ class Or(Rule):
         """
 
         :param rule_id: Unique Rule Id
+        :param rule_description: Rule Description
         :param lhs: Left Hand Side Rule in the AND operation eg:  (rule1 and rule2 where rule1 is lhs rule)
         :param rhs: Right Hand Side Rule in the AND operation eg:  (rule1 and rule2 where rule2 is rhs rule)
         """
-        super(Or, self).__init__(rule_id, rule_description)
+        super(Or, self).__init__(rule_id, rule_description, conditional_rules=None)
         self.lhs = lhs
         self.rhs = rhs
 
-    def execute(self, rule_input):
+    @wrap_rule_exception
+    def execute(self, rule_input, parent_rule=None):
+        parent_rule_id = parent_rule.rule_id if parent_rule else None
+        parent_rule_description = parent_rule.rule_description if parent_rule else None
+
         if (self.lhs and isinstance(self.lhs, Rule)) and (self.rhs and isinstance(self.rhs, Rule)):
-            return self.rule_id, self.lhs.execute(rule_input) or self.rhs.execute(rule_input), "Pass"
+            lhs_rule_id, lhs_rule_status, lhs_rule_status_reason, lhs_parent_rule_id, lhs_parent_rule_description, \
+                lhs_child_rules = self.lhs.execute(rule_input, parent_rule=self)
+            rhs_rule_id, rhs_rule_status, rhs_rule_status_reason, rhs_parent_rule_id, rhs_parent_rule_description, \
+                rhs_child_rules = self.rhs.execute(rule_input, parent_rule=self)
+            overall_status = lhs_rule_status or rhs_rule_status
+            if overall_status:
+                return self.rule_id, self.rule_description, True, "Pass", \
+                       parent_rule_id, parent_rule_description, \
+                       [(lhs_rule_id, lhs_rule_status, lhs_rule_status_reason,
+                         lhs_parent_rule_id, lhs_parent_rule_description, lhs_child_rules),
+                        (rhs_rule_id, rhs_rule_status, rhs_rule_status_reason,
+                         rhs_parent_rule_id, rhs_parent_rule_description, rhs_child_rules)]
+            else:
+                return self.rule_id, self.rule_description, False, "Fail", \
+                       parent_rule_id, parent_rule_description, \
+                       [(lhs_rule_id, lhs_rule_status, lhs_rule_status_reason,
+                         lhs_parent_rule_id, lhs_parent_rule_description, lhs_child_rules),
+                        (rhs_rule_id, rhs_rule_status, rhs_rule_status_reason,
+                         rhs_parent_rule_id, rhs_parent_rule_description, rhs_child_rules)]
         else:
-            return self.rule_id, False, "The left-hand/right-hand side should be of type Rule. " \
-                          "Kindly validate the Rule Config input for Rule Id {} and try again".format(self.rule_id)
+            return self.rule_id, self.rule_description, False, \
+                   "The left-hand/right-hand side should be of type Rule. " \
+                   "Kindly validate the Rule Config input for Rule Id {} and try again".format(self.rule_id), \
+                   parent_rule_id, parent_rule_description, []
+
+
--- rules_engine/rules/rule.py
+++ rules_engine/rules/rule.py
@@ -1,24 +1,47 @@
 from abc import ABC, abstractmethod
+import traceback
+
+
+class RuleException(Exception):
+    pass
+
+
+def wrap_rule_exception(f):
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except (RuleException, TypeError, Exception):
+            parent_rule_id = kwargs['parent_rule'].rule_id if kwargs['parent_rule'] else None
+            parent_rule_description = kwargs['parent_rule'].rule_description if kwargs['parent_rule'] else None
+            return args[0].rule_id, args[0].rule_description, False, \
+                "Exception occurred during the execution of the Rule Id {} :- {}"\
+                .format(args[0].rule_id, traceback.format_exc()), parent_rule_id, parent_rule_description, []
+    return wrapper
 
 
 class Rule(ABC):
 
-    def __init__(self, rule_id, rule_description):
+    def __init__(self, rule_id, rule_description, conditional_rules=None):
         """
 
         :param rule_id: Unique Rule Id
         :param rule_description: Provides the textual description of the Rule
+        :param conditional_rules: List of pre-conditions to be satisfied prior to execution of this Rule
         """
         self.rule_id = rule_id
         self.rule_description = rule_description
+        self.conditional_rules = conditional_rules
 
     @abstractmethod
+    @wrap_rule_exception
     def execute(self, rule_input):
         """
 
         :param rule_input: The Input entity on which the Rule will be applied.
-        :rtype: Should return a tuple i.e (str, boolean, str)
-                eg: (rule_id, True, "Pass")  or (rule_id, False, "Failure reason")
+        :rtype: Should return a tuple i.e (str, boolean, str, str/NoneType)
+                eg: (R456, True, "Pass", R123)  or (R124, False, "Failure reason", None)
         """
         pass
+
+
 
