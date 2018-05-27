4fb75635b619599bac15d2376cd82248fd4676b2
Darryl Fernandes <darrylfernandes@gmail.com>
2018-05-28 00:57:04 +1000

    1) provided a feature to evaluate conditional rules before the execution of the Rule. Unless certain conditions are satisfied, the rule will not be executed

--- rules_engine/util/executor.py
+++ rules_engine/util/executor.py
@@ -17,11 +17,24 @@ class RuleExecutor(object):
         overall_rule_execution_status = True
         overall_rule_execution_status_reason = 'Pass'
         for rule in rules:
-            rule_id, rule_description, status, status_reason, parent_rule_id, parent_rule_description, \
-                children_rule_trace = rule.execute(self.rule_input)
-            rule_execution_trace.append((rule_id, rule_description, status, status_reason,
-                                         parent_rule_id, parent_rule_description, children_rule_trace))
-            if not status:
+            conditional_rule_status = True
+            if rule.conditional_rules:
+                conditional_rule_id, conditional_rule_description, conditional_rule_status, conditional_status_reason, \
+                    conditional_parent_rule_id, conditional_parent_rule_description, conditional_children_rule_trace = \
+                    rule.execute(self.rule_input)
+
+            if conditional_rule_status:
+                rule_id, rule_description, status, status_reason, parent_rule_id, parent_rule_description, \
+                    children_rule_trace = rule.execute(self.rule_input)
+                rule_execution_trace.append((rule_id, rule_description, status, status_reason,
+                                             parent_rule_id, parent_rule_description, children_rule_trace))
+                if not status:
+                    overall_rule_execution_status = False
+                    overall_rule_execution_status_reason = 'Fail'
+            else:
+                rule_execution_trace.append((rule.rule_id, rule.rule_description, False,
+                                             'Conditional Rule criteria to execute the Rule has failed',
+                                             None, None, []))
                 overall_rule_execution_status = False
                 overall_rule_execution_status_reason = 'Fail'
 
